b7ea9fb25c01d81adae84291ded65d74517b9ef2
yattinda <yattinda@gmail.com>
2021-03-15 23:15:06 +0900

    Fix & add comment

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,15 +1,15 @@
 class BonusDrink
   def self.full_bottle_cal(num)
-    #3本飲んで加えられたものと3本セットにならなかったジュース
+    #3本飲んで加えられた飲んでいないジュースとと3本セットにならなかった飲んでいないジュース
     return (num % 3) + (num / 3)
   end
 
   def self.total_count_for(amount)
-    #full_bottle_remainよりfull_bottle_remainのほうが分かりやすい
+    #amountよりfull_bottle_remainのほうが分かりやすい
     full_bottle_remain = amount
     total_bottle = 0
     while full_bottle_remain >= 3 do
-      #飲んだ本数
+      #3本セットで飲んだ本数(余り分は飲まない)
       total_bottle = total_bottle + (full_bottle_remain / 3) * 3
       #飲んでない本数
       full_bottle_remain = full_bottle_cal(full_bottle_remain)
